9e7cc6fa713d89deee8db71fec232dfddd434d51
Pedro Oliveira <pedrogamer212004@gmail.com>
2024-11-21 15:06:43 -0300

    Conexão http com android

--- frontend/src/shared/functions/calcularResultado/calcularResultado.ts
+++ frontend/src/shared/functions/calcularResultado/calcularResultado.ts
@@ -2,53 +2,58 @@ import { Alert } from "react-native";
 
 
 export default async function calcularResultado(inputValue1:any, inputValue2:any, inputValue3:any, setResultado:any) {
-    try {
+    if (inputValue1 == '' || inputValue2 == '' || inputValue3 == '') {
+        Alert.alert("Há campos vazios.")
+    } else{
+        try {
 
-        console.log('1')
+            console.log('1')
 
-        // Verificar se a rede está disponível antes de tentar a requisição
-        const isConnected = await checkNetworkConnectivity();
-        if (!isConnected) {
-            Alert.alert('Erro de conexão', 'Verifique sua conexão com a internet.');
-            return;
-        }
-        
-        // AWS: http//:15.229.5.232/calcular
-        // LOCAL: http://10.0.2.2:5000/calcular
-        const response = await fetch('http://10.0.2.2:5000/calcular', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                n1: parseFloat(inputValue1),
-                n2: parseFloat(inputValue2),
-                operacao: inputValue3,
-            }),
-        });
-        console.log('2')
+            // Verificar se a rede está disponível antes de tentar a requisição
+            const isConnected = await checkNetworkConnectivity();
+            if (!isConnected) {
+                Alert.alert('Erro de conexão', 'Verifique sua conexão com a internet.');
+                return;
+            }
+            
+            // AWS: http//:15.229.5.232/calcular
+            // LOCAL: http://10.0.2.2:5000/calcular
+            const response = await fetch('http://18.228.3.172:5000/calcular', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    n1: parseFloat(inputValue1),
+                    n2: parseFloat(inputValue2),
+                    operacao: inputValue3,
+                }),
+            });
+            console.log('2')
 
-        console.log(response)
+            console.log(response)
 
-        const data = await response.json();  // Recebe a resposta do backend
-        console.log('Resposta do servidor:', data);  // Verifique o que está sendo retornado
+            const data = await response.json();  // Recebe a resposta do backend
+            console.log('Resposta do servidor:', data);  // Verifique o que está sendo retornado
 
-        if (response.ok) {
-            console.log('Resultado do calculo:', data.resultado)
-            setResultado(data.resultado.toString());  // Atualiza o estado com o resultado
-        } else {
-            console.log("Erro: ", data.resultado.toString())
-            Alert.alert(data.error);  // Exibe um erro se houver
-        }
-    } catch (error:any) {
-        if (error.name === 'AbortError') {
-            Alert.alert('Requisição demorou demais e foi cancelada.');
-            console.log("Requisição demorou demais e foi cancelada.")
-        } else {
-            Alert.alert("Erro ao processar a resposta do servidor.");
-            console.error(error);
+            if (response.ok) {
+                console.log('Resultado do calculo:', data.resultado)
+                setResultado(data.resultado.toString());  // Atualiza o estado com o resultado
+            } else {
+                console.log("Erro: ", data.resultado.toString())
+                Alert.alert(data.error);  // Exibe um erro se houver
+            }
+        } catch (error:any) {
+            if (error.name === 'AbortError') {
+                Alert.alert('Requisição demorou demais e foi cancelada.');
+                console.log("Requisição demorou demais e foi cancelada.")
+            } else {
+                Alert.alert("Erro ao processar a resposta do servidor.");
+                console.error(error);
+            }
         }
     }
+
 }
 
 // Função para verificar a conectividade com a internet
